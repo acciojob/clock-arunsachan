aecc42d561da85c872cfba4843a0574fbe7ad53c
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-05-31 18:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function getTime(){
+    const time = new Date();
+    let dayString = `${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
+
+    let hr=time.getHours();
+    let timeString= "";
+
+    if(hr>=12){
+    if(hr === 12){
+        timeString = `${time.getHours()}:${time.getMinutes}:${time.getSeconds} PM`
+    }
+    else{
+      hr -= 12;
+      timeString = `${hr}:${time.getMinutes}:${time.getSeconds} PM`
+    }
+}
+     else{
+    // :AM
+    // 12 hr clock no modification needed 
+    timeString = `${time.getHours()}:${time.getMinutes}:${time.getSeconds} AM`
+}
+   return dayString+ " "+timeString;
+}
+
+const p= document.getElementById('timer');
+
+function timer(){
+    p.innerText = getTime();
+}
+
+setInterval(timer, 1000);
